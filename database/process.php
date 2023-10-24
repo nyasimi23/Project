f6a79035bff62a4866dc1418b0c7bf32282e8987
Jnyasimi <138282130+nyasimi23@users.noreply.github.com>
2023-10-24 15:14:41 +0300

    How to Contact us
    Managed to connect contact page to database, i am able to insert data to databse now!

---
+++ database/process.php
@@ -0,0 +1,33 @@
+
+<?php
+
+$servername = "localhost"; // Replace with your server name
+$username = 'Xhaka'; // Replace with your MySQL username
+$password = '123456'; // Replace with your MySQL password
+$dbname = 'carcrazeconnect'; // Replace with your database name
+
+// Create connection
+$conn = new mysqli($servername, $username, $password,$dbname);
+
+// Check connection
+if ($conn->connect_error) {
+  die('Connection failed: ' . $conn->connect_error);
+}
+
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $subject = $_POST["subject"];
+    $message = $_POST["message"];
+
+    $sql = "INSERT INTO customer_info (name, email, subject, message) VALUES ('$name', '$email', '$subject', '$message')";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "Data inserted successfully.";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+}
+
+?>
